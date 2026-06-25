ceba2f201cff640c20ce1a1e7bd52627cc9f6989
Daniel <souzadaniel187@gmail.com>
2026-06-25 19:17:58 -0300

    fix: move CorsMiddleware to first position in MIDDLEWARE

--- backend/nexo/settings.py
+++ backend/nexo/settings.py
@@ -105,9 +105,9 @@ INSTALLED_APPS = [
 ] + (["django_browser_reload"] if RUNSERVER else [])
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
